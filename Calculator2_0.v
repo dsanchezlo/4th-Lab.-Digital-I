d753ec08291aed8e92e4825d7af88cc86f5ae624
SebasMB12 <82240142+SebasMB12@users.noreply.github.com>
2021-04-16 19:20:54 -0500

    Update Calculator2_0.v

--- Calculator2_0.v
+++ Calculator2_0.v
@@ -7,6 +7,7 @@ module Calculator2_0(clk, col, fil, Sseg, anodos);
 	wire clk1kHz;
 	wire [3:0]tecla;
 	wire tipo;
+	wire [3:0]pulsacion;
 	wire [20:0]Ssegnum1;
 	wire [20:0]Ssegnum2;
 	wire [6:0]signo1;
@@ -17,7 +18,13 @@ module Calculator2_0(clk, col, fil, Sseg, anodos);
 	wire [1:0]oper;
 	wire [19:0]resultado;
 	wire signo_resultado;
-	
+	wire [3:0] dres0;
+	wire [3:0] dres1;
+	wire [3:0] dres2;
+	wire [3:0] dres3;
+	wire [3:0] dres4;
+	wire [3:0] dres5;	
+
 	//Guarda el valor ingresado con la tecla
 	wire [6:0] CSseg;
 	
@@ -25,16 +32,16 @@ module Calculator2_0(clk, col, fil, Sseg, anodos);
 	divfreq div(clk, clk1kHz);
 	
 	Barrido_filas barfil(clk1kHz, fil);
-	Comparador_col comp(fil, col, tecla, tipo);
+	Comparador_col comp(fil, col, tecla, tipo, pulsacion);
 	
 	Conversor print(tecla, tipo, CSseg);
 	
-	Barrido_displays bardisp(CSseg, tipo, clk1kHz, Sseg, anodos, Ssegnum1, Ssegnum2, signo1, operador);
+	Barrido_displays bardisp(CSseg, tipo, clk1kHz, dres0, dres1, dres2, dres3, dres4, dres5, signo_resultado, Sseg, anodos, Ssegnum1, Ssegnum2, signo1, operador);
 	
-	Ssegtobin convsegtobin(Ssegnum1, Ssegnum2, signo1, operador, num1, num2, sig1, oper);
+	/*Ssegtobin convsegtobin(Ssegnum1, Ssegnum2, signo1, operador, num1, num2, sig1, oper);
 	
 	Operacion op(num1, num2, sig1, oper, resultado, signo_resultado);
 	
-	Digitos_result digres(resultado, dres0, dres1, dres2, dres3, dres4, dres5);
+	Digitos_result digres(resultado, dres0, dres1, dres2, dres3, dres4, dres5);*/
 	
-endmodule 
+endmodule
