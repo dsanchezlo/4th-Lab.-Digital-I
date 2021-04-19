75dcc3bbe121c20f3ab233b37ef1099514c1dd47
jsilva09 <82488285+jsilva09@users.noreply.github.com>
2021-04-19 18:03:08 -0500

    Update Barrido_filas.v

--- Barrido_filas.v
+++ Barrido_filas.v
@@ -1,20 +1,20 @@
-module Barrido_filas(clk1kHz, fil);
-	input clk1kHz;
+module Barrido_filas(clk20Hz, fil);
+	input clk20Hz;
 	output reg [3:0] fil;
 	reg [1:0] contador;
 
 	initial begin
-		contador = 2'b00;
+		contador = 'b0;
 	end
 
-	always @(posedge clk1kHz) begin
-		contador = contador + 2'b1;
+	always @(posedge clk20Hz) begin
 		case(contador)
 			'b00: fil = 'b1000;
 			'b01: fil = 'b0100;
 			'b10: fil = 'b0010;
 			'b11: fil = 'b0001;
 		endcase
+		contador = contador + 'b1;
 	end
 
 endmodule
