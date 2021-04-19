5a323ab73e7704ac88c6a7651d53c45f0c1ca327
jsilva09 <82488285+jsilva09@users.noreply.github.com>
2021-04-19 18:09:07 -0500

    Update Operacion.v

--- Operacion.v
+++ Operacion.v
@@ -3,12 +3,12 @@ module Operacion (num1, num2, sig1, oper, resultado, signo_resultado);
 	input [9:0]num2;
 	input sig1;
 	input [1:0]oper;
-	output reg [19:0]resultado;
-	output reg signo_resultado;
+	output reg [19:0] resultado;
+	output reg [1:0] signo_resultado;
 	
 	always @(*)begin
 		case(oper)
-			'd0: 	if (sig1 == 0) begin // Si num 1 es positivo
+			'd0: begin	if (sig1 == 0) begin // Si num 1 es positivo
 						resultado = num1 + num2;
 						signo_resultado = 'd0;
 					end else begin
@@ -20,7 +20,8 @@ module Operacion (num1, num2, sig1, oper, resultado, signo_resultado);
 							signo_resultado = 'd0;
 						end
 					end
-			'd1: 	if (sig1 == 1) begin // Si num1 es negativo
+				end
+			'd1: begin	if (sig1 == 1) begin // Si num1 es negativo
 						resultado = num1 + num2;
 						signo_resultado = 'd1;
 					end else begin
@@ -32,10 +33,25 @@ module Operacion (num1, num2, sig1, oper, resultado, signo_resultado);
 							signo_resultado = 'd0;
 						end
 					end
-			'd2: ;
-			'd3: ;
+				end
+			'd2: begin resultado = num1*num2;
+							if (sig1 == 0) begin // Si num 1 es positivo
+								signo_resultado = 'd0;
+							end else signo_resultado = 'd1;
+			end
+			'd3: begin if(num1 % num2 == 0) begin
+								resultado = num1/num2;
+							
+								if (sig1 == 0) begin // Si num 1 es positivo
+									signo_resultado = 'd0;
+								end else signo_resultado = 'd1;	
+					
+							end else signo_resultado = 'd2;
+			end
 		endcase
 	
+		
+		
 	end
 	
 	
